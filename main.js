9f84f884beaef078d72903ab8c393abbc9dbae8f
epq <aerial.exe@gmail.com>
2022-06-14 21:00:27 +0800

    Converted data attributes

--- main.js
+++ main.js
@@ -4,9 +4,34 @@ const numbersFile = 'data/numbers.csv';
 const stressorsFile = 'data/stressors.csv';
 
 Promise.all([d3.csv(honeyFile), d3.csv(numbersFile), d3.csv(stressorsFile)]).then(files => {
-    const [honey, data, stressors] = files;
+    const [honeyData, numbersData, stressorsData] = files;
+    const honey = honeyData.map(d => {
+        for (prop in d) {
+            if (prop !== 'State') {
+                d[prop] = Number(d[prop]);
+            }
+        }
+        return d;
+    });
+    const numbers = numbersData.map(d => {
+        for (prop in d) {
+            if (prop !== 'State' && prop !== 'Year') {
+                d[prop] = Number(d[prop]);
+            }
+        }
+        return d;
+    });
+    const stressors = stressorsData.map(d => {
+        for (prop in d) {
+            if (prop !== 'State' && prop !== 'Year' && prop !== 'Diseases') {
+                d[prop] = Number(d[prop]);
+            }
+        }
+        return d;
+
+    });
     console.log(honey[0]);
-    console.log(data[0]);
+    console.log(numbers[0]);
     console.log(stressors[0]);
 }).catch(err => {
     console.log(err);
